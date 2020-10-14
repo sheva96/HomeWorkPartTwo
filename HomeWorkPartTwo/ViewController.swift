733096c15b24111e503c0e2a7831e5557111d472
Yevhen Shevchenko <sheva9620@gmail.com>
2020-10-14 12:58:11 +0300

    Release version

--- HomeWorkPartTwo/ViewController.swift
+++ HomeWorkPartTwo/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.layer.cornerRadius = 75
+        redView.alpha = 0.5
+        yellowView.layer.cornerRadius = 75
+        yellowView.alpha = 0.5
+        greenView.layer.cornerRadius = 75
+        greenView.alpha = 0.5
+        
+        startButton.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func actionStartButton() {
+        
+        startButton.setTitle("Next", for: .normal)
+        
+        switch redView.alpha {
+        case 0.5 where yellowView.alpha == 0.5 && greenView.alpha == 0.5:
+            redView.alpha = 1
+        case 1 where yellowView.alpha == 0.5:
+            yellowView.alpha = 1
+            redView.alpha = 0.5
+        case 0.5 where yellowView.alpha == 1:
+            yellowView.alpha = 0.5
+            greenView.alpha = 1
+        case 0.5 where greenView.alpha == 1:
+            greenView.alpha = 0.5
+            redView.alpha = 1
+        default:
+            break
+        }
+    }
 }
-
